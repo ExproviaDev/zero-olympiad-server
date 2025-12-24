48583a4e7ba0e70ddfb04d888d25e6583f93f772
RaselMridha792 <raselmridha792@gmail.com>
2025-12-24 23:50:00 +0600

    implement user quizController for user quiz functionalities, give extra protection when user take quizez

--- router/quizRouter.js
+++ router/quizRouter.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const { createFullQuiz, getAllQuizzes, deleteQuiz, updateQuiz, getSingleQuiz, getQuizzesForUsers,
-    getSingleQuizForUser } = require('../controller/quizController');
+const {
+    createFullQuiz, 
+    getAllQuizzes,
+    deleteQuiz,
+    updateQuiz,
+    getSingleQuiz,
+    getQuizzesForUsers,
+    getSingleQuizForUser,
+    submitQuiz,    
+ } = require('../controller/quizController');
 const { verifyAdmin } = require('../middleware/authMiddleware');
 
 
 
-// সব রাউটে verifyAdmin যোগ করা হলো
 router.get('/all-quizzes', verifyAdmin, getAllQuizzes);
 router.get('/quiz/:id', verifyAdmin, getSingleQuiz);
 router.put('/update-quiz/:id', verifyAdmin, updateQuiz);
@@ -14,8 +21,8 @@ router.post('/add-quiz', verifyAdmin, createFullQuiz);
 router.delete('/delete-quiz/:id', verifyAdmin, deleteQuiz);
 
 
-//for user api
 router.get('/public-quizzes', getQuizzesForUsers);
 router.get('/public-quiz/:id', getSingleQuizForUser);
+router.post('/submit-quiz', submitQuiz);
 
 module.exports = router;
