0778afd80e33cfc8302f4e5e7d5fbefbc7c017ad
RaselMridha792 <raselmridha792@gmail.com>
2026-02-11 15:50:14 +0600

    fix server error

--- router/leaderboardRouter.js
+++ router/leaderboardRouter.js
@@ -1,8 +1,7 @@
-// router/leaderboardRouter.js
 const express = require('express');
 const router = express.Router();
 const { verifyToken } = require('../middleware/authMiddleware');
-const { getLeaderboardData, getLeaderboardStatus } = require('../controller/leaderBoardController');
+const { getLeaderboardData, getLeaderboardStatus } = require('../controller/leaderboardController');
 
 router.get('/',verifyToken, getLeaderboardData);
 router.get('/status', verifyToken, getLeaderboardStatus);
--- router/videoRouter.js
+++ router/videoRouter.js
@@ -1,16 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { verifyToken } = require('../middleware/authMiddleware');
-// ✅ getVideoRoundSettings ইম্পোর্ট করতে ভুলবেন না
 const { submitVideoLink, getVideoStatus, getVideoRoundSettings } = require('../controller/videoController');
-
-// ১. সেটিংস চেক করার রাউট (এটি আপনার মিসিং ছিল)
 router.get('/settings', verifyToken, getVideoRoundSettings);
-
-// ২. ভিডিও সাবমিট করার রাউট
 router.post('/submit', verifyToken, submitVideoLink);
-
-// ৩. ভিডিওর বর্তমান অবস্থা বা মার্ক দেখার রাউট
 router.get('/status/:user_id', verifyToken, getVideoStatus);
 
 module.exports = router;
