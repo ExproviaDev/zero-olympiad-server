067540653782c62f9caacdbb62807542c6fe91a4
RaselMridha792 <raselmridha792@gmail.com>
2026-02-14 12:47:46 +0600

    fix ambassador and email template fixing

--- router/ambassadorRouter.js
+++ router/ambassadorRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 // আপনার মিডলওয়্যার ফাইলের পাথ অনুযায়ী এগুলো ইমপোর্ট করুন
 const { verifyToken, verifyAdmin } = require('../middleware/authMiddleware');
-const { getAllAmbassadors, getReferralList, getAmbassadorSelfStats } = require('../controller/ambassadorController');
+const { getAllAmbassadors, getReferralList, getAmbassadorSelfStats, updatePromoCode } = require('../controller/ambassadorController');
 
 router.get('/all', verifyToken, verifyAdmin, getAllAmbassadors);
 
@@ -10,5 +10,6 @@ router.get('/all', verifyToken, verifyAdmin, getAllAmbassadors);
 router.get('/referrals/:promoCode', verifyToken, getReferralList);
 
 router.get('/my-stats', verifyToken, getAmbassadorSelfStats);
+router.put('/update-code', verifyToken, updatePromoCode);
 
 module.exports = router;
